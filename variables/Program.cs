2e4937559b53d559edd5844f77526b8b0c083a16
Mariusz Bugla <mariusz.bugla@mtime.it>
2022-11-08 16:10:15 +0100

    Add project files.

---
+++ variables/Program.cs
@@ -0,0 +1,18 @@
+﻿int a;
+string b;
+double c;
+
+a = 10;
+b = "to jest tekst";
+c = 10.11;
+
+
+Console.WriteLine("To jest double {0}",c);
+Console.WriteLine("To jest string {0} ", b);
+Console.WriteLine(" to jest int {0} ", a);
+
+a = Int32.Parse(Console.ReadLine());
+b = Console.ReadLine();
+c = Double.Parse(Console.ReadLine());
+
+Console.WriteLine(c + " " + b + " " + a);
